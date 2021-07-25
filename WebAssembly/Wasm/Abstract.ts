1baf06eec7501c4f78efa1fdddf7418b9479de66
xieguigang <xie.guigang@gcmodeller.org>
2021-07-25 19:28:28 +0800

    add source project

---
+++ WebAssembly/Wasm/Abstract.ts
@@ -0,0 +1,21 @@
+﻿namespace vanilla {
+
+    export interface WebAssembly {
+
+        instantiate(module: Uint8Array, dependencies: object): IWasm;
+    }
+
+    export interface IWasm {
+        instance: WasmInstance;
+    }
+
+    export interface WasmInstance {
+        exports: {
+            
+        }
+    }
+
+    export interface WasmMemory {
+        buffer: ArrayBuffer
+    }
+}
